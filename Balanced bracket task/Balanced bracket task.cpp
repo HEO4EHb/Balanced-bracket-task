79f4fbf3d16c0a2ba93f2eb7d3c0fd27456fe8ff
Yan <Yan@DESKTOP-A3ENL6M>
2022-11-04 14:44:51 +0200

    Add project files.

---
+++ Balanced bracket task/Balanced bracket task.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <string>
+#include <stack>
+bool check(const std::string& expression)
+{
+    std::stack<char> stack;
+
+    for (auto ch : expression) {
+        switch (ch) {
+        case '(':
+        case '[':
+        case '{':
+            stack.push(ch);
+            break;
+        case ')':
+            if (stack.empty() || stack.top() != '(') return false;
+            stack.pop();
+            break;
+        case ']':
+            if (stack.empty() || stack.top() != '[') return false;
+            stack.pop();
+            break;
+        case '}':
+            if (stack.empty() || stack.top() != '{') return false;
+            stack.pop();
+            break;
+        }
+    }
+    return stack.empty();
+}
+int main()
+{
+    std::string a = { '(','[','{','}',']',')' };
+    if (check(a)) std::cout << "Right bracket sequence";
+    else std::cout << "False bracket sequence";
+
+    return 0;
+}
